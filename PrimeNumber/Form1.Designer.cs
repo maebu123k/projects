20214e436c46af9005aaa7095a1639f56505d019
maebu123k <101860508+maebu123k@users.noreply.github.com>
2022-04-11 02:47:27 +0800

    Add files via upload

---
+++ PrimeNumber/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace PrimeNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstPrime = new System.Windows.Forms.ListBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstPrime
+            // 
+            this.lstPrime.FormattingEnabled = true;
+            this.lstPrime.Location = new System.Drawing.Point(241, 37);
+            this.lstPrime.Name = "lstPrime";
+            this.lstPrime.Size = new System.Drawing.Size(127, 238);
+            this.lstPrime.TabIndex = 0;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(33, 53);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter the integer greater than one:";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(33, 99);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(100, 23);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generate!";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 305);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lstPrime);
+            this.Name = "Form1";
+            this.Text = "Prime Number";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstPrime;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ PrimeNumber/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeNumber
+{
+    public partial class Form1 : Form
+    {
+        //GLOBAL VARIABLE
+        int input;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //PERFORMS GENERATION OF PRIME NUMBERS FROM 2 TO THE USER INPUT NUMBER
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            //THIS IS A VALIDATION TO CHECK IF THE USER INPUT IS EMPTY
+            try
+            {
+                input = Int32.Parse(txtInput.Text);
+            }
+            catch
+            {
+                //ERROR MESSAGE IS DISPLAYED IF INPUT IS EMPTY
+                MessageBox.Show("Input cannot be blank", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            //DECLARATION OF LIST
+            List<int> numbers = new List<int>();
+
+            //POPULATING THE LIST numbers WITH VALUES FROM 2 TO USER INPUT
+            for (int x = 2; x <= input; x++)
+            {
+                numbers.Add(x);
+            }
+
+            //DECLARATION OF LIST THAT WILL HANDLE THE PRIME NUMBERS
+            List<int> primes = new List<int>();
+
+            //CALLS THE FUNCTION FindAll, PASSES THE numbers LIST
+            //TO GET THE PRIME NUMBERS AND STORES IT IN THE primes LIST
+            primes = FindAll(numbers);
+
+            //ADDS THE PRIME NUMBERS IN THE LIST BOX
+            for (int y = 0; y < primes.Count; y++)
+            {
+                lstPrime.Items.Add(primes.ElementAt(y));
+            }
+        }
+
+        //THE METHOD TO GET FOR THE PRIME NUMBERS
+        private List<int> FindAll(List<int> ls)
+        {
+            //DECLARE LIST THAT CONTAINS THE PRIME NUMBERS
+            List<int> primenumbers = new List<int>();
+
+            //RETRIEVES EACH NUMBERS FROM THE PASSED LIST TO EVALUATE
+            for (int i = ls.ElementAt(0); i <= input; i++)
+            {
+                int counter = 0;
+
+                //TEST THE NUMBER TO DIVIDE FROM THE CURRENT VALUE OF i
+                //DOWN TO 1 AND SEE IF THE REMAINDER IS 0.
+                for (int num = i; num >=1; num--)
+                {
+                    if (i % num == 0)
+                    {
+                        counter = counter + 1;
+                    }
+                }
+
+                //IF THERE ARE TWO MODULUS OPERATION WITH 0 REMAINDER, 
+                //THE NUMBER IS A PRIME NUMBER AND WILL BE ADDED TO THE LIST
+                if (counter == 2)
+                {
+                    primenumbers.Add(i);
+                }
+            }
+            return primenumbers;
+        }
+    }
+}
