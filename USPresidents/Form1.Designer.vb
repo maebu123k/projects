e20b84c07578f623db2bed11f63291adfe06d8c3
maebu123k <101860508+maebu123k@users.noreply.github.com>
2022-04-19 18:45:32 +0800

    Add files via upload

---
+++ USPresidents/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.lstValues = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(116, 24)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(168, 23)
+        Me.btnDisplay.TabIndex = 0
+        Me.btnDisplay.Text = "Display Values"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'lstValues
+        '
+        Me.lstValues.FormattingEnabled = True
+        Me.lstValues.ItemHeight = 15
+        Me.lstValues.Location = New System.Drawing.Point(116, 63)
+        Me.lstValues.Name = "lstValues"
+        Me.lstValues.Size = New System.Drawing.Size(168, 169)
+        Me.lstValues.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(411, 255)
+        Me.Controls.Add(Me.lstValues)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Name = "Form1"
+        Me.Text = "Presidential Ages at Inauguration"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents lstValues As ListBox
+End Class
---
+++ USPresidents/Form1.vb
@@ -0,0 +1,39 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        Dim ages(44) As Integer
+
+        'OMITTED ORIGINAL LINES FROM YOUR CODE
+        'Dim temp() As String = IO.File.ReadAllLines("AgeAtInaug.txt")
+
+        'STORES THE NAMES IN THE FILE
+        Dim names(44) As String
+
+        'SERVES AS A COUNTER FOR THE NUMBER OF LINES READ
+        Dim i As Integer = 0
+
+        'OMITTED ORIGINAL LINES FROM YOUR CODE
+        'For i As Integer = 0 To 44
+
+        'READS EACH LINE OF THE FILE
+        For Each line As String In File.ReadAllLines("AgeAtInaug.txt")
+            'GETS THE NAME OF THE PRESIDENT AND STORES IN THE ARRAY names
+            names(i) = line.Substring(0, line.IndexOf(","))
+            'GETS THE AGE OF THE PRESIDENT AND STORES IN THE ARRAY ages
+            ages(i) = Integer.Parse(line.Substring(line.IndexOf(",") + 1))
+            'PRINTS OUT THE CONTENTS OF THE AGEATINAUG.TXT FILE
+            lstValues.Items.Add(names(i) & ": " & ages(i))
+            'ADDS 1 TO THE LINE COUNTER
+            i = i + 1
+        Next
+        'OMITTED ORIGINAL LINES FROM YOUR CODE
+        'lstValues.Items.Add(names(names.Count - 1) & ": " & ages(ages.Count - 1))
+        'lstValues.Items.Add(names.First & ": " & ages.First)
+        'lstValues.Items.Add(names.Last & ": " & ages.Last)
+
+        'PRINTS OUT THE OLDEST, YOUNGEST AND AVERAGE AGES FROM FILE
+        lstValues.Items.Add("Oldest Age: " & ages.Max)
+        lstValues.Items.Add("Youngest Age: " & ages.Min)
+        lstValues.Items.Add("Average Age: " & (ages.Sum / ages.Count))
+    End Sub
+End Class
